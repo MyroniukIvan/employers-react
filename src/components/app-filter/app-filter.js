a2a028a9cabeb786848f3c891568a32d758ec887
win <vanyamironyuk5@gmail.com>
2022-07-28 10:38:36 -0400

    version-completed

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -1,22 +1,35 @@
 import './app-filter.css'
 
-const appFilter = () => {
-  return (
-      <div className={'btn-group'}>
-          <button className={'btn bnt-light'}
-          type={'button'}>
-              Все сотрудники
-          </button>
-          <button className={'btn bnt-light'}
-                  type={'button'}>
-              На повышение
-          </button>
-          <button className={'btn bnt-light'}
-                  type={'button'}>
-              З/П больше 1000$
-          </button>
-      </div>
-  )
-}
 
-export default appFilter;
+const AppFilter = (props) => {
+        const buttonsData = [
+            {name: 'all', label: 'Все сотрудники'},
+            {name: 'rise', label: 'На повышение'},
+            {name: 'moreThan1000', label: ' З/П больше 1000$'},
+        ];
+
+     const buttons = buttonsData.map(({name,label}) => {
+
+        // if props filter = name / returns true/false
+         const active = props.filter === name;
+        // highlighting active / unactive btns
+         const clazz = active ? "btn-light" : "btn-outline-light";
+
+         return (
+            <button
+                className={`btn ${clazz}`}
+                type={'button'}
+                key={name}
+            onClick={() => props.onFilterSelect(name)}>
+                {label}
+            </button>
+             )
+        })
+
+        return (
+            <div className={'btn-group'}>
+                {buttons}
+            </div>
+        )
+}
+export default AppFilter;
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -13,12 +13,8 @@ class App extends Component {
         super(props);
         this.state = {
             data: [{name: 'John C.', salary: 800, increase: false, rise: true, id: 1}, {
-                name: 'Alex M.',
-                salary: 3000,
-                increase: true,
-                rise: false,
-                id: 2
-            }, {name: 'Carl W.', salary: 5000, increase: false, rise: false, id: 3}]
+                name: 'Alex M.', salary: 3000, increase: true, rise: false, id: 2
+            }, {name: 'Carl W.', salary: 5000, increase: false, rise: false, id: 3}], term: '', filter: '',
         }
         this.maxId = 4;
     }
@@ -31,15 +27,20 @@ class App extends Component {
         })
     }
 
-    // Да, пока могут добавляться пустые пользователи. Мы это еще исправим
-    addItem = (name, salary) => {
+    addItem = (name, salary, items) => {
+        //adding new item to store data features
         const newItem = {
-            name,
-            salary,
-            increase: false,
-            rise:false,
-            id: this.maxId++,
+            name, salary, increase: false, rise: false, id: this.maxId++,
         }
+
+        // Check for not adding empty names and salaries
+        if (name.length < 3 && name.length != null) {
+            return;
+        }
+        if (salary.length < 3 && salary.length !== null) {
+            return;
+        }
+
         this.setState(({data}) => {
             const newArr = [...data, newItem];
             return {
@@ -53,33 +54,63 @@ class App extends Component {
         //возвращаем новый массив через мар, если совпадают ид то возваращаем новый обьект, если не совпадает то возвращаем старый обьект
         // есть -, сетСтате - ассинхронная, выполняется не сразу, при клике на печеньку есть задержка
         this.setState(({data}) => ({
-data: data.map(item => {
-    if ( item.id === id) {
-        return {...item,[prop]: !item[prop] }
-    }
-    return item;
-})
+            data: data.map(item => {
+                if (item.id === id) {
+                    return {...item, [prop]: !item[prop]}
+                }
+                return item;
+            })
         }))
     }
 
+    searchEmp = (items, term) => {
+        if (term.length === 0) {
+            return items;
+        }
+        // take name of every item use indexOf for every of them and return those who are completed by condition > -1
+        return items.filter(item => {
+            return item.name.indexOf(term) > -1
+        })
+    }
 
+    onUpdateSearch = (term) => {
+        this.setState({term})
+    }
+
+    filterPost = (items, filter) => {
+        switch (filter) {
+            case 'rise':
+                return items.filter(item => item.rise)
+            case 'moreThan1000':
+                return items.filter(item => item.salary > 1000);
+            default:
+                return items;
+        }
+    }
+    onFilterSelect = (filter) => {
+        this.setState({filter})
+    }
 
     render() {
+        const {data, term, filter} = this.state;
+        //setting new short way to check employers quantity
         const employers = this.state.data.length;
         const increased = this.state.data.filter(item => item.increase).length;
+        const visibleData = this.filterPost(this.searchEmp(data, term), filter);
         return (<div className="app">
             <AppInfo
-           employers={employers}
-           increased={increased}
+                employers={employers}
+                increased={increased}
             />
-
             <div className="search-panel">
-                <SearchPanel/>
-                <AppFilter/>
+                <SearchPanel onUpdateSearch={this.onUpdateSearch}/>
+                <AppFilter
+                    fiter={filter}
+                    onFilterSelect={this.onFilterSelect}/>
             </div>
 
             <EmployersList
-                data={this.state.data}
+                data={visibleData}
                 onDelete={this.deleteItem}
                 onToggleProp={this.onToggleProp}
             />
